1946f6c9de5ecc34462fe9aee63efd9aee044932
Mostafa Rostam Dokht Effati <mostafa.rostamdokht@gmail.com>
2023-07-21 21:21:05 +0330

    Adding route controllers for upload & download

--- src/routers/index.cjs
+++ src/routers/index.cjs
@@ -2,4 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/download', () =>
+{
+    
+})
+
+router.post('/upload', () =>
+{
+    
+})
+
 module.exports = router
